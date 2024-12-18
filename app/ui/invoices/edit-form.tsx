fd83b8f301c5ce0f4160af2c6a895a687e66e59b
tejasmk-tkp <tejasmk.tkp@gmail.com>
2024-12-18 23:07:44 +0530

    create, edit, delete features for invoice

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,19 +18,20 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
-    <form>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+    <form action={updateInvoiceWithId}>
+      <div className="bg-gray-50 p-4 md:p-6 rounded-md">
         {/* Customer Name */}
         <div className="mb-4">
-          <label htmlFor="customer" className="mb-2 block text-sm font-medium">
+          <label htmlFor="customer" className="block mb-2 font-medium text-sm">
             Choose customer
           </label>
           <div className="relative">
             <select
               id="customer"
               name="customerId"
-              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              className="block border-gray-200 py-2 pl-10 border rounded-md w-full text-sm placeholder:text-gray-500 cursor-pointer outline-2 peer"
               defaultValue={invoice.customer_id}
             >
               <option value="" disabled>
@@ -41,13 +43,13 @@ export default function EditInvoiceForm({
                 </option>
               ))}
             </select>
-            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+            <UserCircleIcon className="top-1/2 left-3 absolute w-[18px] h-[18px] text-gray-500 -translate-y-1/2 pointer-events-none" />
           </div>
         </div>
 
         {/* Invoice Amount */}
         <div className="mb-4">
-          <label htmlFor="amount" className="mb-2 block text-sm font-medium">
+          <label htmlFor="amount" className="block mb-2 font-medium text-sm">
             Choose an amount
           </label>
           <div className="relative mt-2 rounded-md">
@@ -59,19 +61,19 @@ export default function EditInvoiceForm({
                 step="0.01"
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="block border-gray-200 py-2 pl-10 border rounded-md w-full text-sm placeholder:text-gray-500 outline-2 peer"
               />
-              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <CurrencyDollarIcon className="top-1/2 left-3 absolute w-[18px] h-[18px] text-gray-500 peer-focus:text-gray-900 -translate-y-1/2 pointer-events-none" />
             </div>
           </div>
         </div>
 
         {/* Invoice Status */}
         <fieldset>
-          <legend className="mb-2 block text-sm font-medium">
+          <legend className="block mb-2 font-medium text-sm">
             Set the invoice status
           </legend>
-          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
+          <div className="border-gray-200 bg-white px-[14px] py-3 border rounded-md">
             <div className="flex gap-4">
               <div className="flex items-center">
                 <input
@@ -80,13 +82,13 @@ export default function EditInvoiceForm({
                   type="radio"
                   value="pending"
                   defaultChecked={invoice.status === 'pending'}
-                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  className="border-gray-300 bg-gray-100 focus:ring-2 w-4 h-4 text-gray-600 cursor-pointer"
                 />
                 <label
                   htmlFor="pending"
-                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
+                  className="flex items-center gap-1.5 bg-gray-100 ml-2 px-3 py-1.5 rounded-full font-medium text-gray-600 text-xs cursor-pointer"
                 >
-                  Pending <ClockIcon className="h-4 w-4" />
+                  Pending <ClockIcon className="w-4 h-4" />
                 </label>
               </div>
               <div className="flex items-center">
@@ -96,23 +98,23 @@ export default function EditInvoiceForm({
                   type="radio"
                   value="paid"
                   defaultChecked={invoice.status === 'paid'}
-                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  className="border-gray-300 bg-gray-100 focus:ring-2 w-4 h-4 text-gray-600 cursor-pointer"
                 />
                 <label
                   htmlFor="paid"
-                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
+                  className="flex items-center gap-1.5 bg-green-500 ml-2 px-3 py-1.5 rounded-full font-medium text-white text-xs cursor-pointer"
                 >
-                  Paid <CheckIcon className="h-4 w-4" />
+                  Paid <CheckIcon className="w-4 h-4" />
                 </label>
               </div>
             </div>
           </div>
         </fieldset>
       </div>
-      <div className="mt-6 flex justify-end gap-4">
+      <div className="flex justify-end gap-4 mt-6">
         <Link
           href="/dashboard/invoices"
-          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+          className="flex items-center bg-gray-100 hover:bg-gray-200 px-4 rounded-lg h-10 font-medium text-gray-600 text-sm transition-colors"
         >
           Cancel
         </Link>
